2fdc927658342e9a025e3aa3733f9bdd6f83a98e
ismoil <FaxriddinAbdumajidov31072003>
2023-06-01 23:16:08 +0500

    bir

---
+++ script.js
@@ -0,0 +1,28 @@
+var name = +prompt('  4 + 4 = qoshuv amalni bajaring jaxongir aka')
+
+if(name == 8){
+  alert("tog'ri bajardingiz");
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
+var name = +prompt('  4 - 4 = ayruv amalni bajaring')
+
+if(name == 0){
+  alert("tog'ri bajardingiz");
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
+var name = +prompt('  4 * 4 = kopaytruv amalni bajaring')
+
+if(name == 16){
+  alert("tog'ri bajardingiz");
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
+var name = +prompt('  4 / 4 = boluv amalni bajaring')
+
+if(name == 1){
+  alert("tog'ri bajardingiz");
+}else{
+  alert("javob notog'ri qayta urunib koring");
+}
